ee2ef44bedff894adad8e18544c93f0d3fc929be
Wamphyre <34998382+Wamphyre@users.noreply.github.com>
2023-05-01 14:26:53 +0200

    Update core_install.sh

--- core_install.sh
+++ core_install.sh
@@ -25,8 +25,6 @@ else
 fi
 
 install_and_configure() {
-    echo "Backing up the current configuration..."
-    configurations/backup.sh $USER_HOME
     echo "The necessary resources will be installed."
     echo "Please enter your sudo password!"
     sudo resources/install_resources.sh
@@ -34,27 +32,15 @@ install_and_configure() {
     configurations/apply.sh $USER_HOME $EXEC_USER
 }
 
-remove_and_restore() {
-    echo "The installed resources will be removed."
-    echo "Please enter your sudo password!"
-    sudo resources/remove_resources.sh
-    echo "Removing configurations installed."
-    configurations/remove.sh $USER_HOME
-}
-
 # Subcommands and help 
 case "$1" in
     1|install)
         install_and_configure
         ;;
-    2|remove)
-        remove_and_restore
-        ;;
     *)
         echo -e "\n\tUsage: skinpack [command]"
         echo -e "\n\tAvailable commands (You can use name or command number):"
         echo -e "\t\t1 | install   # Install skinpack completely and configure it"
-        echo -e "\t\t2 | remove    # Remove skinpack completely and back to the previous configuration"
         exit 1
         ;;
 esac
